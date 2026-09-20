b271c3c50ecc4beb0c4ce5ba84f777f6f3991082
Talha Ridwan <talharidwan77697@gmail.com>
2026-09-20 23:18:49 +0600

    Just the state machine's jump function built with mutex lock

---
+++ GEMStateMachine.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <mutex>
+#include <unordered_map>
+#include <set>
+enum struct State{
+    OFF_LINE,
+    IDLE,
+    SETUP,
+    EXECUTING,
+    PAUSED,
+    ALARM
+};
+
+struct GEMStateMachine{
+private:
+    State state_;
+    mutable std::mutex mtx_;
+    const unordered_map<State, set<State>> possibleSwitchPair = {
+        {State::OFF_LINE, {State::IDLE}},
+        {State::IDLE, {State::OFF_LINE, State::SETUP, State::ALARM}},
+        {State::SETUP, {State::IDLE, State::EXECUTING, State::ALARM}},
+        {State::EXECUTING, {State::PAUSED, State::IDLE, State::ALARM}},
+        {State::PAUSED, {State::EXECUTING, State::IDLE, State::ALARM}},
+        {State::ALARM, {State::IDLE}}
+    };
+public:
+    GEMStateMachine(State CurrentState = State::OFF_LINE) : 
+        state_(CurrentState){}
+
+    bool switchState(State to){
+        lock_guard<mutex> lock(mtx_);
+        set<State> allowed = possibleSwitchPair.at(state_);
+        if(allowed.find(to) != allowed.end()){
+            state_ = to;
+            return true;
+        }
+        return false;
+    }
+};
